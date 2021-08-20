9ff92562b10077550d1ac7342a5d295bbf570f87
BobosAppol <74260248+BabosSrc@users.noreply.github.com>
2021-08-20 20:53:03 +0800

    Add files via upload

---
+++ src/PlateWheel/ReportUI/Main.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace PlateWheel\ReportUI;
+
+use pocketmine\Server;
+use pocketmine\Player;
+
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+use CortexPE\DiscordWebhookAPI\Webhook;
+use CortexPE\DiscordWebhookAPI\Embed;
+use CortexPE\DiscordWebhookAPI\Message;
+
+class Main extends PluginBase {
+
+	public function onCommand(CommandSender $sender, Command $cmd, String $label, Array $args) : bool {
+
+		switch($cmd->getName()){
+			case "report":
+			 if($sender instanceof Player){
+			 	$this->reportUI($sender);
+			 } else {
+			 	$sender->sendMessage("Soooo?");
+			 }
+			break;
+		}
+	return true;
+	}
+
+	public function reportUI($player){
+		$form = $this->getServer()->getPluginManager()->getPlugin("FormAPI")->createCustomForm(function (Player $player, array $data = null){
+			if($data === null){
+				return true;
+			}
+			if($data[0] == null){
+				$player->sendMessage("Please type the player name");
+				return true;
+			}
+			if($data[1] == null){
+				$player->sendMessage("Please type the reason");
+				return true;
+			}
+			$player->sendMessage("Report has been notice for the staff ywy");
+			foreach($this->getServer()->getOnlinePlayers() as $p){
+				if($p->hasPermission("report.view")){
+					$p->sendMessage("Reportttttt Reportttt\nName: " . $data[0] . "\n Reason: " . $data[1] . "\n Reporter: " . $player->getName() . "\n Ban Ban?");
+				}
+			}
+			// $webhook = new Webhook("https://discord.com/api/webhooks/874904463930953738/xB52UezssKODX4hJVLUUtSTMgDMNc3ZcPp0P-vCK91jCSU08FEvm8QsWEy7n0zZX_b22");
+			// $msg = new Message();
+			// $embed = new Embed();
+			// $embed->setTitle("New report uwuwuwuwuwu");
+			// $embed->addField("Name", $data[0]);
+			// $embed->addField("Reason", $data[1]);
+			// $embed->addField("Reporter", $player->getName());
+			// $embed->setFooter("Ban?");
+			// $msg->addEmbed($embed);
+			// $webhook->send($msg);
+
+			$webhook = new Webhook("my webhook link");
+            $msg = new Message();
+            $embed = new Embed();
+            $embed->setTitle("New report has been sent");
+            $embed->addField("Name:", $data[0]);
+            $embed->addField("Reason:", $data[1]);
+            $embed->addField("Reporter:", $player->getName());
+            $embed->setFooter("Punish?");
+            $msg->addEmbed($embed);
+            $webhook->send($msg);
+
+			$webhook = new Webhook("my webhook link");
+            $msg = new Message();
+            $embed = new Embed();
+            $embed->setTitle("New report has been sent");
+            $embed->addField("Name:", $data[0]);
+            $embed->addField("Reason:", $data[1]);
+            $embed->addField("Reporter:", $player->getName());
+            $embed->setFooter("Punish?");
+            $msg->addEmbed($embed);
+            $webhook->send($msg);
+		});
+		$form->setTitle("ReportUI");
+		$form->addInput("Type a player name you want to report", "Eg: Plaet");
+		$form->addInput("Type a reason why you report", "Eg: for fun :3");
+		$form->sendToPlayer($player);
+		return $form;
+	}
+
+}
